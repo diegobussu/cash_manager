6f207f70c01847bf8c55b1503ccfec508f4db7ca
DontitoSama <jokonito06@gmail.com>
2025-05-13 17:12:37 +0200

    feat: implement getUserByID method in UserService for fetching user details by ID

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,7 @@
 import { jwtDecode } from "jwt-decode";
 import AuthService from "./authServices";
+import { User } from "@/models/User";
+import axios from "axios";
 
 export default class UserService {
   public static async getUserIdFromToken(): Promise<string> {
@@ -10,6 +12,28 @@ export default class UserService {
     } catch (error) {
       console.error("Error decoding token:", error);
       throw new Error("Failed to retrieve user ID from token");
+    }
+  }
+
+  public static async getUserByID(userId: string): Promise<User> {
+    try {
+      const token = await AuthService.getAuthToken();
+      const response = await axios.get(
+        `${AuthService.BASE_URL}/users/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      return response.data as User;
+    } catch (error: any) {
+      console.error("Error fetching user by ID:", error);
+      if (error.response && error.response.data) {
+        throw new Error(error.response.data.message || "Failed to fetch user");
+      }
+      throw new Error("An unexpected error occurred");
     }
   }
 }
