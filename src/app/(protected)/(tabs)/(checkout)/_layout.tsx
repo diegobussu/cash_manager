490c8620da2d51daf1d902d0f36fa6ba57cfc8db
DontitoSama <jokonito06@gmail.com>
2025-05-19 16:43:29 +0200

    feat: add HistoryButton component and integrate it into the Checkout layout

--- src/app/(protected)/(tabs)/(checkout)/_layout.tsx
+++ src/app/(protected)/(tabs)/(checkout)/_layout.tsx
@@ -1,9 +1,14 @@
+import HistoryButton from "@/components/HistoryButton";
 import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Checkout" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Checkout", headerRight: () => <HistoryButton /> }}
+      />
+      <Stack.Screen name="history" options={{ title: "History" }} />
     </Stack>
   );
 }
---
+++ src/components/HistoryButton.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useRouter } from "expo-router";
+
+export default function HistoryButton() {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity onPress={() => router.push("/history")}>
+      <MaterialCommunityIcons name="history" size={22} color="#0853A9" />
+    </TouchableOpacity>
+  );
+}
