ebca2b1822677c5b3effaebdc8d2cb56d849df4e
DontitoSama <jokonito06@gmail.com>
2025-05-27 11:16:26 +0200

    feat: add localization support with English and French translations

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,34 +4,37 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "@/utils/authContext";
 import { CartProvider } from "@/utils/cartContext";
+import { LocalizationProvider } from "@/utils/i18n";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <StatusBar style="auto" />
-        <Stack>
-          <Stack.Screen
-            name="(protected)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="login"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="register"
-            options={{
-              headerShown: false,
-              animation: "slide_from_bottom",
-            }}
-          />
-        </Stack>
-      </CartProvider>
-    </AuthProvider>
+    <LocalizationProvider>
+      <AuthProvider>
+        <CartProvider>
+          <StatusBar style="auto" />
+          <Stack>
+            <Stack.Screen
+              name="(protected)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="login"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="register"
+              options={{
+                headerShown: false,
+                animation: "slide_from_bottom",
+              }}
+            />
+          </Stack>
+        </CartProvider>
+      </AuthProvider>
+    </LocalizationProvider>
   );
 }
